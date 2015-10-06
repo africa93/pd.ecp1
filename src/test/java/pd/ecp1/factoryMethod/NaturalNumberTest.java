ccd371b72801f388125387e04ce0e2c04351c532
África <africa_93@hotmail.com>
2015-10-06 13:35:24 +0200

    Creada la clase test para NaturalNumber

---
+++ src/test/java/pd/ecp1/factoryMethod/NaturalNumberTest.java
@@ -0,0 +1,14 @@
+package pd.ecp1.factoryMethod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NaturalNumberTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
