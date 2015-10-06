0a09b8fb4a79238a2535790853fc92d9f47df647
África <africa_93@hotmail.com>
2015-10-06 09:21:01 +0200

    Initial

---
+++ src/main/java/pd/ecp1/singleton/ReferencesFactory.java
@@ -0,0 +1,36 @@
+package pd.ecp1.singleton;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReferencesFactory {
+	private static ReferencesFactory referencesFactory = new ReferencesFactory();
+	
+    private Map<String, Integer> references;
+
+    private int reference;
+
+    private ReferencesFactory() {
+        this.references = new HashMap<>();
+        this.reference = 0;
+    }
+    
+    public static ReferencesFactory getFactory(){
+    	return referencesFactory;
+    }
+
+    public int getReference(String key) {
+        Integer result = this.references.get(key);
+        if (result == null) {
+            this.references.put(key, this.reference);
+            result = this.reference;
+            reference++;
+        }
+        return result;
+    }
+
+    public void removeReference(String key) {
+        this.references.remove(key);
+    }
+
+}
