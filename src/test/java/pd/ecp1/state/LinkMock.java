46e1b69595230f850ea4f3bbf4efffdc0ebe3a0e
África <africa_93@hotmail.com>
2015-10-06 10:12:59 +0200

    Añadido el Test de state y LinkMock

---
+++ src/test/java/pd/ecp1/state/LinkMock.java
@@ -0,0 +1,27 @@
+package pd.ecp1.state;
+
+
+public final class LinkMock implements Link {
+    private String msg;
+
+    private int respuesta;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public int getRespuesta() {
+        return respuesta;
+    }
+
+    @Override
+    public void enviar(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public void recibir(int respuesta) {
+        this.respuesta = respuesta;
+    }
+
+}
